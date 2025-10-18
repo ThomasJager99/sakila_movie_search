ef83a675843c710fa337b9dc71b63d8ee38639f2
ThomasJager99 <artemzubchenko1@gmail.com>
2025-10-18 15:18:53 +0200

    Still trying to create first validator

--- validation.py
+++ validation.py
@@ -6,6 +6,38 @@ from pydantic import AliasChoices
 from pydantic import ValidationError
 from pydantic import ConfigDict
 from pydantic import field_validator
+from config import _mysql_conn
+from pymysql import cursors
+import json
+
+
+# ====================================================================================================
+#NOTE: trying to fetchall genres from my database to understand better validation method
+# def genre_list():
+#     sql='''SELECT category_id, name FROM category ORDER BY name;'''
+#     with _mysql_conn() as conn, conn.cursor() as cur:
+#         cur.execute(sql),
+#         rows= cur.fetchall()
+#     return json.dumps(rows, indent=4)
+# print(genre_list())
+#====================================================================================================
+#NOTE: exported all genres to .json file in order to create own little db so i dont need to use conn
+# for validation system every time and can use this genres like recomendation for user
+# def genre_list(filename="genres.json"):
+#     sql= '''  SELECT c.category_id, c.name, MIN(f.release_year) AS first_year, MAX(f.release_year) AS last_year
+#         FROM category AS c
+#         JOIN film_category AS fc ON fc.category_id = c.category_id
+#         JOIN film AS f ON f.film_id = fc.film_id
+#         GROUP BY c.category_id, c.name
+#         ORDER BY c.name;
+#          '''
+#     with _mysql_conn() as conn, conn.cursor() as cur:
+#         cur.execute(sql)
+#         rows = list(cur.fetchall())
+#     with open(filename, "w", encoding="utf-8") as f:
+#         json.dump(rows, f, indent=4, ensure_ascii=False)
+#     print(f"Exported {len(rows)} genres to {filename}")
+#====================================================================================================
 
 class Key_word_validation(BaseModel):
     model_config = ConfigDict(
@@ -38,6 +70,7 @@ def main():
             print(f"[invalid] {loc}: {err['msg']}")
 
 if __name__ == "__main__":
+    # genre_list()
     main()
 
 
