e7297f4b61d711d21fe917a79574bc042d7c884b
ThomasJager99 <artemzubchenko1@gmail.com>
2025-10-18 12:25:26 +0200

    Change gitignore a bit and create validation madule, but its empty for now

---
+++ validation.py
@@ -0,0 +1,29 @@
+import pydantic
+from pydantic import BaseModel
+from pydantic import Field
+from pydantic import EmailStr
+from pydantic import ValidationError
+
+class Key_word_validation(BaseModel):
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
